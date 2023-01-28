6a3e6c5a0b4b4fe91e9eaacfd5346bc0f267658f
michalk <mkinformacja@gmail.com>
2023-01-28 22:11:56 +0100

    Add Share module, add layout component. Refine routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', loadChildren: () => import('./public/home/home.module').then(m => m.HomeModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
