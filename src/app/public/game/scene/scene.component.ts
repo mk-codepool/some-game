20efe28a79310bd52131c79c407f02fe3d39df4c
michalk <mkinformacja@gmail.com>
2023-01-30 20:05:07 +0100

    Change Phaser to ThreeJS. Prepare default scene.

--- src/app/public/game/scene/scene.component.ts
+++ src/app/public/game/scene/scene.component.ts
@@ -1,37 +1,46 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import * as Phaser from 'phaser';
-import { MainScene } from 'src/app/engine/game';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as THREE from 'three';
+import { Scene } from 'three';
 
 @Component({
   selector: 'app-scene',
   templateUrl: './scene.component.html',
-  styleUrls: ['./scene.component.scss']
+  styleUrls: ['./scene.component.scss'],
 })
 export class SceneComponent implements AfterViewInit {
-  public phaserGame!: Phaser.Game;
-  
-  @ViewChild('gameContainer', { static: true }) gameContainer!: ElementRef;
+  public scene: Scene = new Scene();
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef;
+  @ViewChild('canvasContainer', { static: true }) canvasContainer!: ElementRef;
 
-  public ngAfterViewInit() {
-    const gameConfig: Phaser.Types.Core.GameConfig = this.getGameConfig();
-    this.phaserGame = new Phaser.Game(gameConfig);
-  }
+  ngAfterViewInit() {
+    const { clientWidth, clientHeight } = this.canvasContainer.nativeElement;
+    this.canvas.nativeElement.setAttribute('height', clientHeight);
+    this.canvas.nativeElement.setAttribute('width', clientWidth);
 
-  private getGameConfig(): Phaser.Types.Core.GameConfig {
-    const element = this.gameContainer.nativeElement;
-    
-    return {
-      type: Phaser.AUTO,
-      height: element.offsetHeight - 1,
-      width: element.offsetWidth,
-      scene: [ MainScene ],
-      parent: 'gameContainer',
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 0 }
-        }
-      }
-    };
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      clientWidth / clientHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas.nativeElement,
+    });
+    renderer.setSize(clientWidth, clientHeight);
+
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+    const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    scene.add(cube);
+
+    camera.position.z = 5;
+    renderer.render(scene, camera);
   }
 }
