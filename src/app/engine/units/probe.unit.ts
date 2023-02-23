6334d32ae356159cae8af4b58c4c63634988358f
michalk <mkinformacja@gmail.com>
2023-02-23 16:40:48 +0100

    Refine code of sample probe unit.

--- src/app/engine/units/probe.unit.ts
+++ src/app/engine/units/probe.unit.ts
@@ -9,55 +9,58 @@ export class ProbeUnit {
   constructor() {
     this.setMesh();
   }
+  
+  public update() {
+    TWEEN.update();
+  }
 
   private setMesh(): void {
     // create a sphere geometry with the given radius
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const geometry = new THREE.SphereGeometry(.3, 32, 32);
 
     // create a standard material with the given color
     const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
     // create a mesh using the geometry and material
     this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.position.set(0, 2, 0);
+    this.mesh.castShadow = true;
 
     // initialize the movement speed and key state variables
-    this.speed = 5;
+    this.speed = 1;
 
     // add an event listener to track keyboard key state
     document.addEventListener('keydown', (event) => {
+      const moveTime = 200;
       if (!this.moveTween) {
         // start a new moveTween animation based on the key input
         switch (event.code) {
           case 'ArrowUp':
             this.moveTween = new TWEEN.Tween(this.mesh.position)
-              .to({ z: this.mesh.position.z - this.speed }, 1000)
+              .to({ z: this.mesh.position.z - this.speed }, moveTime)
               .onComplete(() => (this.moveTween = null))
               .start();
             break;
           case 'ArrowDown':
             this.moveTween = new TWEEN.Tween(this.mesh.position)
-              .to({ z: this.mesh.position.z + this.speed }, 1000)
+              .to({ z: this.mesh.position.z + this.speed }, moveTime)
               .onComplete(() => (this.moveTween = null))
               .start();
             break;
           case 'ArrowLeft':
             this.moveTween = new TWEEN.Tween(this.mesh.position)
-              .to({ x: this.mesh.position.x - this.speed }, 1000)
+              .to({ x: this.mesh.position.x - this.speed }, moveTime)
               .onComplete(() => (this.moveTween = null))
               .start();
             break;
           case 'ArrowRight':
             this.moveTween = new TWEEN.Tween(this.mesh.position)
-              .to({ x: this.mesh.position.x + this.speed }, 1000)
+              .to({ x: this.mesh.position.x + this.speed }, moveTime)
               .onComplete(() => (this.moveTween = null))
               .start();
             break;
         }
       }
     });
-  }
-  
-  update() {
-    TWEEN.update();
   }
 }
